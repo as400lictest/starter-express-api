9cc43192e6ae42ba24051a6a9d1e9a618dd7349e
as400lictest <128790159+as400lictest@users.noreply.github.com>
2023-03-24 12:32:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,11 @@ app.use(basicAuth({
     challenge: true
 }));
 
-// Set up HTTPS options
-const options = {
-  key: fs.readFileSync('path/to/key.pem'),
-  cert: fs.readFileSync('path/to/cert.pem')
-};
-
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/license.lic');
 });
 
 // Create HTTPS server and listen on port 3000
-https.createServer(options, app).listen(3000, function() {
+https.createServer(app).listen(3000, function() {
   console.log('App listening on port 3000 with HTTPS and Basic Auth!');
 });
