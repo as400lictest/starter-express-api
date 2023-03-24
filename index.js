504a636d17354b03054d7f3bf59f75269b5ffdd9
as400lictest <128790159+as400lictest@users.noreply.github.com>
2023-03-24 12:35:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const https = require('https');
 const basicAuth = require('express-basic-auth');
 
 // Set up Basic Auth middleware
@@ -14,7 +12,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/license.lic');
 });
 
-// Create HTTPS server and listen on port 3000
-https.createServer(app).listen(3000, function() {
-  console.log('App listening on port 3000 with HTTPS and Basic Auth!');
+app.listen(3000, function() {
+  console.log('App listening on port 3000 with Basic Auth!');
 });
